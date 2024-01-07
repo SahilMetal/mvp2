48f9ddf09bc6b5427de33e577e35552fbeb96dcd
Sahil Metalwala <homefolder@Sahils-MBP.lan>
2024-01-07 14:40:21 -0600

    mongoose db connected and schema created

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const express = require('express')
+const mongoose = require('mongoose');
 const app = express()
 require('dotenv').config()
-const { PORT, DB_URI } = process.env;
+const { PORT, DB_URI } = process.env
+
+const teamSchema = new mongoose.Schema({
+    title: String,
+    tier: String,
+    creator: String,
+    tags: Array
+})
+
 
 app.get('/', function (req, res) {
     res.send('Hello World')
 })
 
-app.listen(3000, () => {
+mongoose.connect(DB_URI)
+    .then(() => console.log('MongoDB connected'))
+    .catch((err) => console.log(err));
+
+app.listen(PORT, () => {
     console.log('mvp2 listening on ', PORT)
 })
